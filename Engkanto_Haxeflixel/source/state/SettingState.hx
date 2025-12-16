693575d1aafe0aaa547bd165a1ab8485289bed4a
x40arcade <sobrepenakenneth97@gmail.com>
2025-12-16 11:03:46 +0800

    Add initial state classes and move documentation
    Moved Engkanto_Docu.docx into the Engkanto_Haxeflixel directory. Added initial CreditState, MenuState, and SettingState Haxe files as subclasses of FlxState, providing basic structure for future implementation.

---
+++ Engkanto_Haxeflixel/source/state/SettingState.hx
@@ -0,0 +1,15 @@
+package state;
+
+import flixel.FlxState;
+
+class SettingState extends FlxState {
+    override public function create():Void {
+        super.create();
+
+        
+    }
+
+    override public function update(elapsed:Float):Void {
+        super.update(elapsed);
+    }
+}
